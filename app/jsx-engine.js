4e92b93094b1e7e7c372378a504b54dd647fe126
Laurent Dezitter <laurent.dezitter@gmail.com>
2015-09-26 09:46:19 +0200

    [app] Clear views from cache
    The .jsx views are loaded by the custom render engine through a require() call
    and thus are cached by node's module system.
    
    This means subsequent requests will use the same component implementation,
    reloading the page will not reflect code changes.
    
    By clearing all JSX components from the cache in development we can see the
    changes with a single page reload (instead of restarting the server)
    
    See https://nodejs.org/api/globals.html#globals_require_cache

--- app/jsx-engine.js
+++ app/jsx-engine.js
@@ -1,11 +1,21 @@
 import path from 'path';
 import React from 'react';
 
+function clearRequireCache() {
+    Object.keys(require.cache)
+          .filter( p => p.endsWith('.jsx') )
+          .forEach( p => delete require.cache[p] );
+}
+
 export default function(componentPath, options, callback) {
     let layoutPath = path.join(this.root, 'layout.jsx');
 
     let LayoutClass = require(layoutPath);
     let ComponentClass = require(componentPath);
+
+    if (!options.cache) {
+        clearRequireCache();
+    }
 
     let component = React.createElement(ComponentClass, options.props);
     let page = React.createElement(
